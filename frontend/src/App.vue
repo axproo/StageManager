a88c420447d61053b1bad858e5d179381bfd7d8b
Asma Chaouat <chaouat.asma@gmail.com>
2026-07-13 12:20:08 +0100

    update test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="min-h-screen flex items-center justify-center">
     <h1 class="text-4xl font-bold text-blue-600">
-      Tailwind fonctionne 🎉
+      welcome asma 🎉
     </h1>
   </div>
 </template>
