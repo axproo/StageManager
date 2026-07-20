62bc5fb07b57d893c0559649861897c6f54180e8
Mohamed Kaabi <hamakaabi904@gmail.com>
2026-07-20 12:24:19 +0200

    Add logout action to user store

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -1,13 +1,23 @@
 import { defineStore } from "pinia";
 
 export const useUserStore = defineStore("user", {
+
   state: () => ({
     username: "",
   }),
 
+
   actions: {
+
     login(name: string) {
       this.username = name;
     },
+
+
+    logout() {
+      this.username = "";
+    }
+
   },
+
 });
